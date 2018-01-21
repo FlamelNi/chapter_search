e2e5de45efcab6cb74ab43df9d76b477dec96a35
FlamelNi <joshua7176@naver.com>
2018-01-21 07:17:33 -0800

    html rework

--- __init__.py
+++ __init__.py
@@ -7,12 +7,22 @@ from pyt import getImportantKeys
 
 app = Flask(__name__)
 
+def getSectionLink(i):
+    return {
+        1: "https://www.khanacademy.org/science/physics/forces-newtons-laws",
+        2: "https://www.khanacademy.org/science/physics/work-and-energy",
+        3: "https://www.khanacademy.org/science/physics/thermodynamics"
+    }.get(i, "https://www.google.com")
+
+def getSectionName(i):
+    return {
+        1: "Force/Motion",
+        2: "Energy",
+        3: "Thermodynamics"
+    }.get(i, "Force/Motion")
+
+
 def getKeyWords():
-    # currently, this returns dummy data
-    # return [ ["worda", 1, 1],
-    #     ["wordb", 3, 2],
-    #     ["wordc", 2, 2],
-    #     ["wordd", 2, 1] ]
     return getImportantKeys()
     
     
@@ -49,12 +59,14 @@ def submit():
             countedSelectedWords.append([ chosen[0], chosen[1], 1 ])
     
     #test print
-    # for alreadyCounted in countedSelectedWords:
-    #     print(alreadyCounted[0] + " " + str(alreadyCounted[1]) + " " + str(alreadyCounted[2]) )
+    for alreadyCounted in countedSelectedWords:
+        print(alreadyCounted[0] + " " + str(alreadyCounted[1]) + " " + str(alreadyCounted[2]) )
     
-    # hello my name is Jay. worda is awesome and wordc too. wordb is the best but I like worda better
     
-    return 'Done'
+    # getSection with machine learning
+    section = 2
+    
+    return render_template('element.html', message = getSectionName(section), link = getSectionLink(section))
 
 
 if __name__ == "__main__":
