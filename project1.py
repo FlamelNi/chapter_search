928d2df13506f2c473e83e22e23a2f16c0300c03
jspark971 <jspark971@gmail.com>
2018-01-21 08:40:22 -0800

    modified project file

--- project1.py
+++ project1.py
@@ -1,8 +1,21 @@
 from pyt import *
 import pandas as pd
 
-cheatsheet = getImportantKeys()
 
+#-----------------------------------------------------------
+#Y-List
+
+def make_y_list():
+
+	sample_quiz_solution = {"Section": ['D','D','C','C','B','B','A','A']}
+	df = pd.DataFrame(sample_quiz_solution)
+	dummies = pd.get_dummies(df)
+	answer_list = dummies.values.tolist()
+
+	return answer_list
+
+#----------------------------------------------------------
+#X-List
 
 
 
